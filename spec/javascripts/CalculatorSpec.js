bed2b10c639395a2e52706eeb2e90679d95c6f21
Hector Correa <hector@hectorcorrea.com>
2015-09-04 15:17:46 -0400

    Adds Jasmine-jquery to test JavaScript that interacts with the DOM via jQuery

--- spec/javascripts/CalculatorSpec.js
+++ spec/javascripts/CalculatorSpec.js
@@ -10,3 +10,25 @@ describe("Calculator", function() {
   });
 
 });
+
+
+describe("Calculator UI", function() {
+
+  var calculator;
+  beforeEach(function() {
+    // Mimic the HTML controls that we need
+    html = '<input id="number1" value="4"/> ' +
+           '<input id="number2" value="3"/> ' +
+           '<span id="resultGoesHere"/>';
+    setFixtures(html);
+    // Execute the JavaScript
+    calculator = new Calculator();
+    calculator.calculateResult();
+  });
+
+  it("updates the UI with the result", function() {
+    result = $("#resultGoesHere");
+    expect(result.text()).toEqual("7");
+  });
+
+});
